0879f295e01c6c343d5954bfb3f4b724382bb591
Jindrich Mynarz <mynarzjindrich@gmail.com>
2013-11-21 16:05:00 +0100

    Load fixture helper method

--- spec/Helpers.rb
+++ spec/Helpers.rb
@@ -1,8 +1,18 @@
 module Helpers
-  # @param file_path [String]
-  # @returns [RDF::Graph]
+  # Loads fixture from `file_name` in /spec/fixtures directory
   #
-  def load_file(file_path)
-    RDF::Graph.load(file_path, format: :ttl)
+  # @param file_name [String]
+  # @returns [RDF::Graph, String] `RDF::Graph` for Turtle input, `String` for other file formats
+  # @raise [ArgumentError]        Error for non-existent `file_name`
+  #
+  def load_fixture(file_name)
+    file_path = File.join(Rails.root, "spec", "fixtures", file_name)
+    raise ArgumentError, "File #{file_path} doesn't exist" unless File.exist? file_path
+
+    if File.extname(file_path) == ".ttl"
+      RDF::Graph.load(file_path, format: :ttl)
+    else
+      File.read file_path
+    end
   end
 end
