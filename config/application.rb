3e6ae24ab560f747c0257dc2c6a4ca3bfc8b5000
Jindrich Mynarz <mynarzjindrich@gmail.com>
2014-04-11 13:54:14 +0200

    Update I18N config

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,9 @@ module Validator
         exit
       end
     end
- 
+
+    config.i18n.enforce_available_locales = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
